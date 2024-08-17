da3b12604524890c337e90e3ad9d8ad58ae8a4ca
norby.dev <127006485+devnorb@users.noreply.github.com>
2024-08-16 19:49:41 -0700

    Update tutorial.cpp

--- tutorial.cpp
+++ tutorial.cpp
@@ -5,12 +5,13 @@ int main()
 {
     std::this_thread::sleep_for(std::chrono::seconds(13)); // give ample time to get into a game (you can modify this value based on your Mac's performance)
     uintptr_t offset = 0x1010adc10;
-    auto print = reinterpret_cast<void(*)(int, const char*, ...)>(_dyld_get_image_vmaddr_slide(0) + (uintptr_t)offset);
+    typedef int (*printdef)(int, const char*, ...);
+    auto printfunction = reinterpret_cast<printdef>(_dyld_get_image_vmaddr_slide(0) + (uintptr_t)offset);
 
-    print(0, "print"); // white
-    print(1, "info"); // blue -> doesn't have chunkname (like Stack Begin/End)
-    print(2, "warn"); // yellow
-    print(3, "error"); // red -> doesn't have chunkname (like Stack Begin/End)
+    printfunction(0, "print"); // white
+    printfunction(1, "info"); // blue -> doesn't have chunkname (like Stack Begin/End)
+    printfunction(2, "warn"); // yellow
+    printfunction(3, "error"); // red -> doesn't have chunkname (like Stack Begin/End)
 
     return 0;
 }
