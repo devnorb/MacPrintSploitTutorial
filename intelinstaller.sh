a590cfb9ca6e511076f10f868803d5c1f3780f3e
xybytz <95513381+xybytz@users.noreply.github.com>
2024-08-17 10:57:45 -0700

    Update intelinstaller.sh

--- intelinstaller.sh
+++ intelinstaller.sh
@@ -4,10 +4,12 @@ main() {
     local json=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
     local version=$(echo "$json" | grep -o '"clientVersionUpload":"[^"]*' | grep -o '[^"]*$')
     curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
     unzip -o -q "./RobloxPlayer.zip"
+    mv "./RobloxPlayer.app" "/Applications/Roblox.app"
     rm "./RobloxPlayer.zip"
-    xattr -c Roblox.app
-    codesign --remove-signature "Roblox.app/Contents/MacOS/RobloxPlayer" 
+    xattr -c /Applications/Roblox.app # removes it from quarantine 
+    codesign --remove-signature "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" # need to remove the signature to make the injection method work
 }
 
 main
