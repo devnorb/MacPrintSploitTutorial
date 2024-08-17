102f2bba1f4f31cbd7ae2e48536d891d78c6a5a6
norby.dev <127006485+devnorb@users.noreply.github.com>
2024-08-16 19:04:38 -0700

    Create tutorial.cpp

---
+++ tutorial.cpp
@@ -0,0 +1,21 @@
+#include <thread>
+#include <mach-o/dyld.h>
+
+int main()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(13)); // give ample time to get into a game (you can modify this value based on your Mac's performance)
+    auto print = reinterpret_cast<void(*)(int, const char*, ...)>(_dyld_get_image_vmaddr_slide(0) + 0x1010adc10);
+
+    print(0, "print"); // white
+    print(1, "info"); // blue -> doesn't have chunkname (like Stack Begin/End)
+    print(2, "warn"); // yellow
+    print(3, "error"); // red -> doesn't have chunkname (like Stack Begin/End)
+
+    return 0;
+}
+
+__attribute__((constructor)) void entry()
+{
+    printf("[ Custom printsploit dylib injected ]\n");
+    std::thread(main).detach();
+}
